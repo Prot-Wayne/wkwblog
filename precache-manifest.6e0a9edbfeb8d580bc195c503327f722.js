9de8acedec8ecc0fdc831161d6aa6f757a04234a
Leonardo <guerrero8315@hotmail.com>
2019-04-02 20:22:30 -0500

    first

---
+++ precache-manifest.6e0a9edbfeb8d580bc195c503327f722.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "12cb423c2383a8ad2f94",
+    "url": "/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "c3ad4841d4991fec76af",
+    "url": "/static/js/2.c3ad4841.chunk.js"
+  },
+  {
+    "revision": "12cb423c2383a8ad2f94",
+    "url": "/static/js/main.12cb423c.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "c3ad4841d4991fec76af",
+    "url": "/static/css/2.7ae9cbc6.chunk.css"
+  },
+  {
+    "revision": "ccd8958a4955deeff911e04f792e6408",
+    "url": "/index.html"
+  }
+];
